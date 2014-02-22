d5c93757e852e97d8ee9ff22d760773689af0078
YangMann <zhangyang93@rocketmail.com>
2014-02-22 14:31:38 +0800

    basic accelerometer

--- aero/src/edu/SJTU/yangmann/aero/MyActivity.java
+++ aero/src/edu/SJTU/yangmann/aero/MyActivity.java
@@ -1,9 +1,22 @@
 package edu.SJTU.yangmann.aero;
 
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.FloatMath;
+import android.util.Log;
 
-public class MyActivity extends Activity {
+public class MyActivity extends Activity implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private float mAccel;
+    private float mAccelCurrent;
+    private float mAccelLast;
+
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +24,42 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        mAccel = 0.00f;
+        mAccelCurrent = SensorManager.GRAVITY_EARTH;
+        mAccelLast = SensorManager.GRAVITY_EARTH;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
+            float[] mGravity = event.values.clone();
+            mAccelLast = mAccelCurrent;
+            mAccelCurrent = FloatMath.sqrt(mGravity[0]* mGravity[0] + mGravity[1]* mGravity[1] + mGravity[2]* mGravity[2]);
+            mAccel = mAccel * 0.9f + mAccelCurrent - mAccelLast;
+            Log.v("mAccel", "[Gx]" + mGravity[0] + "\t[Gy]" + mGravity[1] + "\t[Gz]" + mGravity[2]);
+            if (mAccel > 3) {
+                Log.wtf("mAccel", "SHAKE!!!!");
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
